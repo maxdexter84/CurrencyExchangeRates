10be39406784e0325863eff29a953fabd7f958e2
maxdexter <Maximusnew84!>
2021-03-16 11:59:51 +0300

    made small refactoring

---
+++ app/src/main/java/com/example/currencyexchangerates/ui/bookmark/BookmarkViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.currencyexchangerates.ui.bookmark
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class BookmarkViewModel : ViewModel() {
+
+
+}
