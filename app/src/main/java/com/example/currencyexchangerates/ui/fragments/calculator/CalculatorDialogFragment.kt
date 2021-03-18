f6bc822d5f40fbc277835a624e68de62c9f2a2be
maxdexter <Maximusnew84!>
2021-03-18 11:51:05 +0300

    made and added a calculator

--- app/src/main/java/com/example/currencyexchangerates/ui/fragments/calculator/CalculatorDialogFragment.kt
+++ app/src/main/java/com/example/currencyexchangerates/ui/fragments/calculator/CalculatorDialogFragment.kt
@@ -5,7 +5,10 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.currencyexchangerates.R
 import com.example.currencyexchangerates.databinding.FragmentCalculatorDialogBinding
 
@@ -14,18 +17,39 @@ class CalculatorDialogFragment : BottomSheetDialogFragment() {
 
     private lateinit var binding: FragmentCalculatorDialogBinding
 
+    private val viewModel: CalculatorViewModel by lazy {
+        ViewModelProvider(this).get(CalculatorViewModel::class.java)
+    }
+
+    private val args: CalculatorDialogFragmentArgs? by lazy {
+        arguments?.let { CalculatorDialogFragmentArgs.fromBundle(it) }
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_calculator_dialog,container, false)
 
+
+        initCalculate()
+        initTV()
         return binding.root
     }
 
+    private fun initTV() {
+        binding.tvCurrencyName.text = args?.uiCurrency?.name
+        viewModel.calculateRes.observe(viewLifecycleOwner, {
+            binding.tvValute.text = it
 
+        })
+    }
 
-
-
-
+    private fun initCalculate() {
+        val nominal = args?.uiCurrency?.nominal ?: ""
+        val value = args?.uiCurrency?.value ?: ""
+        binding.etRub.doAfterTextChanged {
+            viewModel.calculate(nominal, value, it.toString())
+        }
+    }
 
 
 }
