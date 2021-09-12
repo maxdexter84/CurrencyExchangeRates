ef9b50bbfabe80e465ec9a9b84a45c0aabb1d943
maxdexter <maxdexter@bk.ru>
2021-09-12 16:12:10 +0300

    add work manager

--- app/src/main/java/com/example/currencyexchangerates/App.kt
+++ app/src/main/java/com/example/currencyexchangerates/App.kt
@@ -1,6 +1,12 @@
 package com.example.currencyexchangerates
 
 import android.app.Application
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequest
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import com.example.currencyexchangerates.data.worker.CurrencyLoadWorker
+import java.util.concurrent.TimeUnit
 
 
 class App : Application() {
@@ -15,5 +21,11 @@ class App : Application() {
         super.onCreate()
         instance = this
         preferences = AppPreferences(applicationContext)
+
+
+
+        val myWork = PeriodicWorkRequestBuilder<CurrencyLoadWorker>(24, TimeUnit.HOURS).build()
+        WorkManager.getInstance(this)
+            .enqueueUniquePeriodicWork("Load Data Work", ExistingPeriodicWorkPolicy.KEEP, myWork)
     }
 }
